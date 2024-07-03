47d9f76657cde33827884bea84e9b48c3efe3d5c
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-07-03 13:50:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+const allStrongElements=document.querySelectorAll('p strong')
+
 function highlight() {
     //Write your code here
-
+	for(const strongElement of allStrongElements){
+		strongElement.style.color='green'
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	for(const strongElement of allStrongElements){
+		strongElement.style.color='black'
+	}
 }
